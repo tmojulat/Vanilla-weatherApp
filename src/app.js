9bb8104920dce4aa52ec5190510627e57d1143c7
tmojulat <140878712+tmojulat@users.noreply.github.com>
2023-08-20 12:35:28 +0800

    Corrected the error in the apiUrl

--- src/app.js
+++ src/app.js
@@ -58,7 +58,9 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "1fa00e65cf105b1c9ae0d3141eca3cfd";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `
+    "https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid={1fa00e65cf105b1c9ae0d3141eca3cfd}&units=metric"`;
+
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -81,7 +83,7 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
